9e400474efc543c87a2bfe8f88f4275ad5d1aa5c
jorgielyn <irangabriellef14@gmail.com>
2019-09-09 08:48:54 +0800

    Updated controller

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,90 +1,40 @@
-//broker
-var btnConnect = document.getElementById('btnConnect');
-var btnDisConnect = document.getElementById('btnDisconnect');
-var broker = document.getElementById('broker');
-var status = document.getElementById('statusCheck');
+var client;
+var address = $("#broker").val();
+$(document).ready(function () {
 
-//publisher
-var btnPublish = document.getElementById('btnPublish');
-var pubTopic = document.getElementById('pubTopic');
-var pubPayload = document.getElementById('pubPayload');
+  $("#btnConnect").click(function () {
+    client = mqtt.connect(address);
+    connected = true;
+    $("#status").text("Connecting....")
+    client.on("connect", function () {
+      console.log("Connected")
+      $("#status").text("Connected!!!!!")
+    })
 
-//subscriber
-var subTopic = document.getElementById('subTopic');
-var btnSubscribe = document.getElementById('btnSubscribe');
-var btnUnsubscribe = document.getElementById('btnUnsubscribe');
+    client.on("message", function (topic, payload) {
+      console.log([topic, payload].join(": "));
+      $('#brokerBody').append('<tr><td>' + topic + '<td>' + payload + '<td>'+moment().format('MMMM Do YYYY, h:mm:ss a') + '</td></tr>');
+    })
 
+    $("#btnPublish").click(function () {
+      console.log("Publish button clicked");
+      var topic = $("input[name=topic]").val();
+      var payload = $("input[name=payload]").val();
+      client.publish(topic, payload)
+      $('#publishBody').append('<tr><td>' + topic + '<td>' + payload + '<td>'+moment().format('MMMM Do YYYY, h:mm:ss a') + '</td></tr>');
+      console.log(topic, payload)
+    })
 
-//btnConnect
-btnConnect.addEventListener('click', function (e) {
-  e.preventDefault();
-  //client
-  $("#statusCheck").text('Connecting....');
-  var client = mqtt.connect(broker.value)
-  // client.subscribe("mqtt/demox")
-
-  btnSubscribe.addEventListener('click', function (e) {
-    e.preventDefault();
-    console.log("mqtt/" + subTopic.value)
-    client.subscribe("mqtt/" + subTopic.value);
-    btnUnsubscribe.disabled = false;
-    btnSubscribe.disabled = true;
+    $("#btnSubscribe").click(function () {
+      console.log("Subscribe button clicked");
+      var subTopic = $("input[name=subTopic]").val();
+      console.log(subTopic);
+      $('#subscribeBody').append('<tr><td>' + subTopic + '<td>'+moment().format('MMMM Do YYYY, h:mm:ss a') + '</td></tr>');
+      client.subscribe(subTopic);
+    });
   })
 
-  btnUnsubscribe.addEventListener('click', function (e) {
-    e.preventDefault();
-    client.unsubscribe("mqtt/" + subTopic.value);
-    btnUnsubscribe.disabled = true;
-    btnSubscribe.disabled = false;
-    console.log("Unsubscribe to mqtt/" + subTopic.value)
-  })
-
-  client.on("connect", function () {
-    console.log("Successfully connected");
-    btnDisConnect.disabled = false;
-    btnConnect.disabled = true;
-    $("#statusCheck").text('Connected!');
-  });
-
-
-  //btnDisconnect
-  btnDisConnect.addEventListener('click', function () {
-    client.end();
-    btnDisConnect.disabled = true;
-    btnConnect.disabled = false;
-    console.log('Disconnected');
-    $("#statusCheck").text('Disconnected!');
-  });
-
-
-  client.on("message", function (topic, payload) {
-    let finalTopic = topic.slice(5);
-    console.log([finalTopic, payload].join(": "));
-    let tbl = document.getElementById('receiver');
-    let tbody = document.getElementById('msg');
-    let tr = document.createElement('tr');
-    let msgTopic = document.createElement('td');
-    let msgPayload = document.createElement('td');
-    let msgTime = document.createElement('td');
-    msgTopic.appendChild(document.createTextNode(finalTopic));
-    msgPayload.appendChild(document.createTextNode(payload));
-    msgTime.appendChild(document.createTextNode(moment().format('llll')));
-    tr.appendChild(msgTopic);
-    tr.appendChild(msgPayload);
-    tr.appendChild(msgTime);
-    tbody.appendChild(tr);
-    tbl.appendChild(tbody);
-    // $('.broker tbody').append("<tr><td>" + finalTopic + "</td><td>" + payload + "</td><td>" + moment().format('llll') + "</td></tr>");
-  })
-
-  // client.publish("mqtt/demox", "hello world!")
-
-  btnPublish.addEventListener('click', function (e) {
-    e.preventDefault();
-    client.publish("mqtt/" + pubTopic.value, pubPayload.value)
-  })
 });
-
 
 
 
